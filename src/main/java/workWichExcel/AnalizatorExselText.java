bfde561a52e5869f711d2ca4182bfd8ca3628ea0
Olexandr Rudyy <rudojsru@gmail.com>
2018-10-20 22:27:06 +0200

    add text to table Excel

---
+++ src/main/java/workWichExcel/AnalizatorExselText.java
@@ -0,0 +1,55 @@
+package workWichExcel;
+
+import workWichTextTxtFiles.TextWriterReader;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class AnalizatorTEST {
+    public static void main(String[] args) throws IOException {
+        ExcelWriter excelWriter = new ExcelWriter();
+        TextWriterReader textWriterReader = new TextWriterReader();
+        List l = textWriterReader.listModifiPhone(); // List from TextWriter
+        Map m = excelWriter.exselWriterMap();  // Maps from Exsel Writer
+
+        for (int i = 0; i < l.size(); i++) {     // List size for беребрать evry elements
+            String listWordText = (String) l.get(i);
+            //       System.out.println(listWordText+"-------------------");
+            String numberOrTextFromList[] = listWordText.split("");
+            if ((numberOrTextFromList[0].equals("0") != true) && (numberOrTextFromList[1].equals("7") != true)) {
+
+                for (Object key : m.keySet()) { // взять новый обьект из  MAP
+                    String mapWord = (String) key;
+                    if (mapWord.equals(listWordText)) {  //in List exist FELDS name from Maps
+                        listWordText = (String) l.get(++i);
+                        numberOrTextFromList = listWordText.split("");
+                        while ((numberOrTextFromList[0].equals("0") == true) && (numberOrTextFromList[1].equals("7") == true)) { // пока в списке телефон, то добавляем его в Мап
+
+
+                            List l1= (List<String>) m.get(mapWord);
+                            l1.add(listWordText);
+                             m.put(mapWord,l1);
+                            if (i == l.size() - 1) {
+                                break;
+                            }
+
+                            listWordText = (String) l.get(++i);
+                            numberOrTextFromList = listWordText.split("");
+
+
+                        }
+                        i--;
+                        break;
+                    }
+
+                }
+            }
+        }
+        System.out.println(m);
+    }
+
+
+}
+
---
+++ src/main/java/workWichExcel/Test.java
@@ -0,0 +1,4 @@
+package workWichExcel;
+
+public class Test {
+}
